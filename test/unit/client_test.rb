e2e2ddfc10d3de2fdc313faf06580262718827c2
Stafford Horne <shorne@gmail.com>
2013-02-15 23:04:22 -0500

    add password manip, fix room format, add a test

--- test/unit/client_test.rb
+++ test/unit/client_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "room format manip" do
+     client = Client.new
+     client.oneldk = 1
+     assert_equal "oneldk", client.room_format
+     
+     client.oneldk = 1
+     client.onedk = 1
+     assert_equal "oneldk,onedk", client.room_format
+     
+     client.onedk = 0
+     assert_equal "oneldk", client.room_format 
+  end
 end
